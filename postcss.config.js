319168dd128c76ff9e52adcd52b7c91dfeb001e6
emareaitch <dev.emareaitch@gmail.com>
2025-06-01 15:53:19 +0530

    Added css minification

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     '@tailwindcss/nesting': {},  // flatten nested rules
     "@tailwindcss/postcss": {},   // ← new plugin
-    autoprefixer: {}             // add vendor prefixes
+    autoprefixer: {},             // add vendor prefixes
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   }
 }
