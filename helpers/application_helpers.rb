34d648410e29ffd4b4d242d9d8d6fe489bafaff4
emareaitch <dev.emareaitch@gmail.com>
2025-06-09 17:02:15 +0530

    Fixed default image

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,2 +1,15 @@
 module ApplicationHelpers
+  def netlify_image_url(article, fm: "avif", fit: nil, w: nil, h: nil, position: nil, q: nil)
+    # fallback image path
+    default_image = '/images/default_article_cover.avif'
+    cover_image = article.data['cover_image']
+
+    # use image_path helper if available
+    image = image_path(cover_image.present? ? cover_image : default_image)
+
+    query_params = default_params.merge(fm:, fit:, w:, h:, position:, q:, url: image).compact
+
+
+    "/.netlify/images?#{query_params.to_query}"
+  end
 end
